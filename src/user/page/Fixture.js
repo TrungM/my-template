65edce9c949e26c62a02d3e5bd0df09124daded0
Le Minh Trung <139935169+MT-Trung@users.noreply.github.com>
2023-10-06 15:16:14 +0700

    10-6-2023

--- src/user/page/Fixture.js
+++ src/user/page/Fixture.js
@@ -65,7 +65,6 @@ const Fixture = () => {
                                                 </div>
                                             </li>
                                         }
-
                                     })}
                                 </ul>
                             </div>
